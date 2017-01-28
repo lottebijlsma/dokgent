428cdfb697639df777c31ab2449d9ff3bb8a4c8d
lottebijlsma <bijlsmalotte@gmail.com>
2017-01-28 17:53:26 +0100

    :lipstick: Responsive detail page

--- src/view/events/detail.php
+++ src/view/events/detail.php
@@ -5,7 +5,7 @@
   <div class='event-content'>
     <img class="event-detail-image" src="../assets/img/database/<?php echo($events['img']);?>" alt="afbeelding van evenement" width="600"/>
     <div class='event-detail-about'>
-      <div>
+      <div class='mobile-position-change'>
         <div class='desc-about'>
           <h3><?php echo($events['title']);?></h3>
           <p><?php echo($events['description']);?></p>
